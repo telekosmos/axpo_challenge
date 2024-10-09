dbc6a6d1a6681c09689c018601035197db86fa8b
telekosmos <dar.es.balat@gmail.com>
2024-10-09 11:28:21 +0200

    chore: remove useless packages and comments

--- axpo_challenge/main.py
+++ axpo_challenge/main.py
@@ -1,8 +1,4 @@
-import pandas as pd
-import streamlit as st
-
-from axpo_challenge.domain.data_retriever import DataRetriever # , load_assets, load_measurements, load_signals
-# from axpo_challenge.deps.logger import logger
+from axpo_challenge.domain.data_retriever import DataRetriever
 from axpo_challenge.app.main import App
 
 retriever = DataRetriever()
